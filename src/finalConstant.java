26b2318c92395f0523f276eee2438663d5757306
ParkJooahn <ParkJooahn@192.168.0.228>
2021-07-06 20:26:53 +0900

    Initial Commit

---
+++ src/finalConstant.java
@@ -0,0 +1,14 @@
+
+public class finalConstant {
+
+	public static void main(String[] args) {
+		final double AGE;
+		AGE = 12.3;
+				System.out.println(AGE);
+		final double LEFT;
+		LEFT = 100 - AGE;
+				System.out.println(LEFT);
+
+	}
+
+}
